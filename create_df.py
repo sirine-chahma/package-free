0a7e3ab8c53023847377f5b58b7a73132d028029
sirine-chahma <54771513+sirine-chahma@users.noreply.github.com>
2020-05-16 00:44:15 -0700

    add map, select market vs store and day

--- create_df.py
+++ create_df.py
@@ -1,8 +1,19 @@
 import pandas as pd
 
 #Data frame to store the locations
-loc_df = pd.DataFrame({'id':['01', 'M01'], 'Name':['Nada', 'West End'],
-                       'lat':[49.2638, 49.2828], 'lon':[-123.0895, -123.1302],
-                       'shop':[1, 0]})
+loc_df = pd.DataFrame({'id': ['01', 'M01'], 'Name': ['Nada', 'West_End'],
+                       'lat': [49.2638, 49.2828], 'lon': [-123.0895, -123.1302],
+                       'shop': [1, 0], 'day': ['{0, 1, 2, 3, 4, 5, 6}', {1}]})
 
 loc_df.to_csv('data/loc.csv')
+
+open_days_int = [0, 2]
+
+loc_df['is_open'] = list(map(lambda x: not set(x).isdisjoint(open_days_int), loc_df['day']))
+
+loc_df = loc_df[loc_df['is_open']]
+
+print(loc_df)
+
+
+
--- src/app.py
+++ src/app.py
@@ -7,96 +7,123 @@ import altair as alt
 import pandas as pd
 from plotly import graph_objs as go
 from plotly.graph_objs import *
-
-
+import plotly.express as px
 
 app = dash.Dash(__name__, assets_folder='assets', external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 
-# Plotly mapbox public token
-mapbox_access_token = "pk.eyJ1IjoicGxvdGx5bWFwYm94IiwiYSI6ImNrOWJqb2F4djBnMjEzbG50amg0dnJieG4ifQ.Zme1-Uzoi75IaFbieBDl3A"
+app.title = 'my_app'
 
-#import data
+# Plotly mapbox public token
+token = "pk.eyJ1Ijoic2lyaW5lY2hhaG1hIiwiYSI6ImNrYTkycnRrNzBuZWgycnF2c2Z6OTJxcWcifQ.-hvKGxFnevCwRy5EKDK9VA"
+
+# import data
 loc_df = pd.read_csv('../data/loc.csv')
 
-#layout of the app
+days_option = [{'label': 'Monday', 'value': 0},
+               {'label': 'Tuesday', 'value': 1},
+               {'label': 'Wednesday', 'value': 2},
+               {'label': 'Thursday', 'value': 3},
+               {'label': 'Friday', 'value': 4},
+               {'label': 'Saturday', 'value': 5},
+               {'label': 'Sunday', 'value': 6}]
 
-app.layout = html.Div(className="map",
-                    children=[
-                        dcc.Graph(id="map-graph"),
-                        ],
-                    )
+# layout of the app
 
-#Map
+app.layout = html.Div(
+    [
+        html.Div(
+            [
+                html.Div(
+                    [
+                        html.P("Type of shop:", className="control_label"),
+                        dcc.RadioItems(
+                            id="shop_selector",
+                            options=[
+                                {"label": "All ", "value": "all"},
+                                {"label": "Store ", "value": "store"},
+                                {"label": "Market ", "value": "market"},
+                            ],
+                            value="all",
+                            labelStyle={"display": "inline-block"},
+                            className="dcc_control",
+                        ),
+                        html.P("Open one of the following :", className="control_label"),
+                        dcc.Dropdown(
+                            id="open_days",
+                            options=days_option,
+                            multi=True,
+                            value=[i for i in range(7)],
+                            className="dcc_control",
+                        ),
+                    ]
+                ),
+            ]
+        ),
+        html.Div(
+            [
+                html.Div(
+                    [dcc.Graph(id="map-graph")],
+                ),
+            ]
+        )
+    ]
+)
+
+
+# Update the map
 @app.callback(
     Output("map-graph", "figure"),
+    [
+        Input("shop_selector", "value"),
+        Input("open_days", "value"),
+    ],
 )
-def update_graph():
-    zoom = 12.0
-    latInitial = 1
-    lonInitial = 1
-    bearing = 0
+def make_map_figure(shop_selector, open_days):
+    if shop_selector == 'store':
+        df = loc_df[loc_df['shop'] == 1]
+    elif shop_selector == 'market':
+        df = loc_df[loc_df['shop'] == 0]
+    else:
+        df = loc_df
+
+    #Select only the stores that are opened
+    open_days_int = list(map(lambda x : int(x), open_days))
+    df['is_open'] = list(map(lambda x: not set(x).isdisjoint(open_days_int), df['day']))
+
+    df = df[df['is_open']]
+
+    gb = df.groupby(['shop'])
+    group_name = list(set(df['shop']))
+    fig = go.Figure()
+    for gr in group_name:
+        my_gr = gb.get_group(gr)
+
+        fig.add_trace(go.Scattermapbox(lon=my_gr['lon'],
+                                       lat=my_gr['lat'],
+                                       name= 'store' if gr == 1 else 'market',
+                                       mode='markers',
+                                       marker=dict(size=14, color='pink' if gr == 1 else 'black',),
+                                       text=my_gr['Name'],
+                                       showlegend=True,
+                                       ))
 
 
-    return go.Figure(
-        data=[
-            # Plot of important locations on the map
-            Scattermapbox(
-                lat=[1, 2],
-                lon=[1, 2],
-                mode="markers",
-                hoverinfo="text",
-                text=[name for name in loc_df['Name']],
-                marker=dict(size=8, color="#ffa0a0"),
+    fig.update_layout(
+        autosize=True,
+        hovermode='closest',
+        mapbox=dict(
+            accesstoken=token,
+            bearing=0,
+            center=dict(
+                lat=49.27,
+                lon=-123.12
             ),
-        ],
-        layout=Layout(
-            autosize=True,
-            margin=go.layout.Margin(l=0, r=35, t=0, b=0),
-            showlegend=False,
-            mapbox=dict(
-                accesstoken=mapbox_access_token,
-                center=dict(lat=latInitial, lon=lonInitial), 
-                style="dark",
-                bearing=bearing,
-                zoom=zoom,
-            ),
-            updatemenus=[
-                dict(
-                    buttons=(
-                        [
-                            dict(
-                                args=[
-                                    {
-                                        "mapbox.zoom": 12,
-                                        "mapbox.center.lon": "1",
-                                        "mapbox.center.lat": "1",
-                                        "mapbox.bearing": 0,
-                                        "mapbox.style": "dark",
-                                    }
-                                ],
-                                label="Reset Zoom",
-                                method="relayout",
-                            )
-                        ]
-                    ),
-                    direction="left",
-                    pad={"r": 0, "t": 0, "b": 0, "l": 0},
-                    showactive=False,
-                    type="buttons",
-                    x=0.45,
-                    y=0.02,
-                    xanchor="left",
-                    yanchor="bottom",
-                    bgcolor="#323130",
-                    borderwidth=1,
-                    bordercolor="#6d6d6d",
-                    font=dict(color="#FFFFFF"),
-                )
-            ],
+            pitch=0,
+            zoom=10
         ),
     )
-
+    return fig
 
 
 if __name__ == "__main__":
