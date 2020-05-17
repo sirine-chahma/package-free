39bcaeab28833e46e6691e5011b9de7375e1fc4c
sirine-chahma <54771513+sirine-chahma@users.noreply.github.com>
2020-05-16 23:58:09 -0700

    add ranking

--- create_df.py
+++ create_df.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
 #Data frame to store the locations
-loc_df = pd.DataFrame({'id': ['01', 'M01'], 'Name': ['Nada', 'West_End'],
+loc_df = pd.DataFrame({'id': ['01', 'M01'], 'Name': ['Nada', 'West End'],
                        'lat': [49.2638, 49.2828], 'lon': [-123.0895, -123.1302],
                        'shop': [1, 0], 'day': [[0, 1, 2, 3, 4, 5, 6], [1]]})
 
 loc_df.to_csv('data/loc.csv')
 
 food_df = pd.DataFrame({'id': ['01', 'M01', '01'],
-                        'Name': ['Nada', 'West_End', 'Nada'],
+                        'Name': ['Nada', 'West End', 'Nada'],
                         'food': ['pasta', 'rice', 'rice'],
                         'min_price': [1.2, 4.5, 2],
                         'avg_price': [2, 5, 3],
--- src/app.py
+++ src/app.py
@@ -5,6 +5,8 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import altair as alt
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 from plotly import graph_objs as go
 from plotly.graph_objs import *
 import plotly.express as px
@@ -35,8 +37,7 @@ food_option = [{'label': 'Rice', 'value': 'rice'},
 
 importance = [{'label': 'Lower price', 'value': 'low'},
               {'label': 'Higher price', 'value': 'high'},
-              {'label': 'Average price', 'value': 'avg'},
-              {'label': 'Distance', 'value': 'dist'}]
+              {'label': 'Average price', 'value': 'avg'}]
 
 # layout of the app
 
@@ -73,11 +74,12 @@ app.layout = html.Div(
                             multi=True,
                             className="dcc_control",
                         ),
+                        html.P("What is the most important?", className="control_label"),
                         dcc.Dropdown(
                             id="importance_dropdown",
                             options=importance,
                             placeholder="Select the most important for you",
-                         ),
+                        ),
                     ]
                 ),
             ]
@@ -113,16 +115,16 @@ def make_map_figure(shop_selector, open_days, food):
     else:
         df = loc_df
 
-    #Select only the stores that are opened
+    # Select only the stores that are opened
     open_days_str = list(map(lambda x: str(x), open_days))
     df['is_open'] = list(map(lambda x: any(i in x for i in open_days_str), df['day']))
     df = df[df['is_open']]
 
-    #Select only the stores with the food that we want
+    # Select only the stores with the food that we want
     if not type(food) == type(None):
-        #counts contains the number of aliment in food that each store has
+        # counts contains the number of aliment in food that each store has
         counts = pd.DataFrame(food_df[food_df['food'].isin(list(food))]['id'].value_counts())
-        #we only keep the store having all the aliments we are looking for
+        # we only keep the store having all the aliments we are looking for
         df = df[df['id'].isin(counts[counts['id'] == len(list(food))].index)]
 
     gb = df.groupby(['shop'])
@@ -133,13 +135,12 @@ def make_map_figure(shop_selector, open_days, food):
 
         fig.add_trace(go.Scattermapbox(lon=my_gr['lon'],
                                        lat=my_gr['lat'],
-                                       name= 'store' if gr == 1 else 'market',
+                                       name='store' if gr == 1 else 'market',
                                        mode='markers',
-                                       marker=dict(size=14, color='pink' if gr == 1 else 'black',),
+                                       marker=dict(size=14, color='pink' if gr == 1 else 'black', ),
                                        text=my_gr['Name'],
                                        showlegend=True,
                                        ))
-
 
     fig.update_layout(
         autosize=True,
@@ -168,9 +169,7 @@ def make_map_figure(shop_selector, open_days, food):
         Input("importance_dropdown", "value")
     ],
 )
-
 def make_ranking(shop_selector, open_days, food, importance):
-
     if shop_selector == 'store':
         df = loc_df[loc_df['shop'] == 1]
     elif shop_selector == 'market':
@@ -178,16 +177,16 @@ def make_ranking(shop_selector, open_days, food, importance):
     else:
         df = loc_df
 
-    #Select only the stores that are opened
+    # Select only the stores that are opened
     open_days_str = list(map(lambda x: str(x), open_days))
     df['is_open'] = list(map(lambda x: any(i in x for i in open_days_str), df['day']))
     df = df[df['is_open']]
 
-    #Select only the stores with the food that we want
+    # Select only the stores with the food that we want
     if not type(food) == type(None):
-        #counts contains the number of aliment in food that each store has
+        # counts contains the number of aliment in food that each store has
         counts = pd.DataFrame(food_df[food_df['food'].isin(list(food))]['id'].value_counts())
-        #we only keep the store having all the aliments we are looking for
+        # we only keep the store having all the aliments we are looking for
         loc_select = df[df['id'].isin(counts[counts['id'] == len(list(food))].index)]
         food_select = food_df[food_df['id'].isin(counts[counts['id'] == len(list(food))].index)]
         # only keep the aliments that we want
@@ -196,9 +195,11 @@ def make_ranking(shop_selector, open_days, food, importance):
         loc_select = df
         food_select = food_df
 
-    #lists to store the final price/distance for each store id
+    # lists to store the final price/distance for each store id
     name = []
     price = []
+    if type(importance) == type(None):
+        importance = 'low'
 
     gb = food_select.groupby(['Name'])
     group_id = list(set(df['Name']))
@@ -206,23 +207,31 @@ def make_ranking(shop_selector, open_days, food, importance):
         name.append(gr)
         my_gr = gb.get_group(gr)
         if importance == 'low':
-            price.append(my_gr['min_price'].mean())
+            price.append(my_gr['min_price'].sum())
         elif importance == 'high':
-            price.append(my_gr['max_price'].mean())
+            price.append(my_gr['max_price'].sum())
         else:
-            price.append(my_gr['avg_price'].mean())
+            price.append(my_gr['avg_price'].sum())
 
     results = pd.DataFrame({'Name': name, 'price': price})
-    results.sort_values(['price'], ascending=False)
+    results = results.sort_values(['price'], ascending=False)
+    results.reset_index(inplace=True)
 
     data = [
         go.Scatter(
             x=results['price'],
-            y=[loc_select.shape[0] - i for i in range(loc_select.shape[0])],
+            y=[i / loc_select.shape[0] for i in range(loc_select.shape[0])],
             mode='markers',
-            marker=dict(color='red', size=10)
+            marker=dict(color='red', size=10),
+            hovertemplate=
+            '<i>Price</i>: CAD%{x:.2f} <br><extra></extra>' +
+            '<b>Name : </b><b>%{text}</b>',
+            text=results['Name']
+
         )
     ]
+
+    results = results.sort_values(['price'], ascending=False)
 
     # Use the 'shapes' attribute from the layout to draw the vertical lines
     layout = go.Layout(
@@ -231,19 +240,61 @@ def make_ranking(shop_selector, open_days, food, importance):
             xref='x',
             yref='y',
             x0=0,
-            y0=loc_select.shape[0] - i,
+            y0=i / loc_select.shape[0],
             x1=results['price'][i],
-            y1=loc_select.shape[0] - i,
+            y1=i / loc_select.shape[0],
             line=dict(
-                color='grey',
+                color='black',
                 width=3
             )
         ) for i in range(loc_select.shape[0])],
-        title='Lollipop Chart'
+        title='Final ranking'
     )
+
+    dic_importance = {'low': 'lowest', 'high': 'highest', 'avg': 'average'}
 
     # Plot the chart
     fig = go.Figure(data, layout)
+    fig.update_yaxes(showticklabels=False)
+    fig.update_layout(
+        xaxis=dict(
+            showline=True,
+            showgrid=False,
+            showticklabels=True,
+            linewidth=2,
+            ticks='outside',
+            title='Expected total ' + dic_importance[importance] + ' final price (in CAD)',
+        ),
+        yaxis=dict(
+            showgrid=False,
+            zeroline=False,
+            showline=False,
+            showticklabels=False,
+        ),
+        autosize=False,
+        plot_bgcolor='white'
+    )
+
+    annotations = []
+
+    for i in range(loc_select.shape[0]):
+        # labeling the left_side of the plot
+        annotations.append(dict(xref='paper', x=0, y=i / loc_select.shape[0],
+                                xanchor='right', yanchor='middle',
+                                text=results['Name'][i],
+                                font=dict(family='Arial',
+                                          size=16),
+                                showarrow=False))
+
+    fig.update_layout(annotations=annotations)
+
+    fig.update_layout(
+        hoverlabel=dict(
+            bgcolor="white",
+            font_size=16,
+            font_family="Rockwell"
+        )
+    )
 
     return fig
 
